49dbe0b166cd153df09931c409e1aac9ce3c6ae5
Albena Kr <albena.krasteva@gmail.com>
2020-01-24 23:35:50 +0000

    Initial setup, links to Mongo and materialize

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 from os import path
 if path.exists("env.py"):
@@ -7,9 +9,15 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'berlin_dictionary'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/get_words')
+def get_words():
+    return render_template("words.html", words=mongo.db.words.find() )
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP","0.0.0.0"), 
