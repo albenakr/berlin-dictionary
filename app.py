471bbb53dfeaab26a47872fe5c30565499e8bf48
Albena Kr <albena.krasteva@gmail.com>
2020-01-26 15:07:55 +0000

    Progress on managewords, more work to be done

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for 
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 from os import path
 if path.exists("env.py"):
-  import env 
+  import env
 
 app = Flask(__name__)
 
@@ -14,14 +14,17 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_words')
 def get_words():
-    return render_template("words.html", words=mongo.db.words.find() )
+    return render_template("words.html", words=mongo.db.words.find())
+
 
 @app.route('/add_word')
 def add_word():
     return render_template('addword.html')
+
 
 @app.route('/insert_word', methods=['POST'])
 def insert_word():
@@ -37,14 +40,12 @@ def insert_word():
     words.insert_one(req)
     return redirect(url_for('get_words'))
 
-@app.route('/manage_words')
-def manage_words():
-    return render_template('managewords.html')
+@app.route('/manage_word/<word_id>')
+def manage_word(word_id):
+    the_word = mongo.db.words.find_one({"_id": ObjectId(word_id)})
+    print(the_word)
+    return render_template('manageword.html', word=the_word)
 
-@app.route('/edit_word/<word_id>')
-def edit_word(word_id):
-    the_word = mongo.db.tasks.find_one({"_id": ObjectId(word_id)})
-    return render_template('/edit_word', word=the_word)
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP","0.0.0.0"), 
